6ea0df1e35bb7f8df0ccc1a03f9d966e8274091a
Irgendeinwer <irgendeinwer@proton.me>
2024-08-19 20:12:34 +0200

    Added Rebuilding Script (thanks Vartroc)

---
+++ scripts/rebuild.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p bash
+
+set -e
+echo -e "This script will test your configuration, add everything in ~/dotfiles/ to git, commit the changes and rebuild your system\n"
+font="rozzo"
+
+figlet -tc -f $font Testing... | lolcat
+read -rp "Enter options to pass to the command (default: none): " options
+sudo nixos-rebuild test --flake ~/dotfiles/#junixos $options
+echo -e "\n"
+
+figlet -tc -f $font Committing... | lolcat
+cd ~/dotfiles/
+git add * 
+read -rp "Enter git commit message: " message
+git commit -m "$message" -a
+echo -e "\n"
+
+figlet -tc -f $font Applying... | lolcat
+sudo nixos-rebuild switch --flake ~/dotfiles/#junixos $options
