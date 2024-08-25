6b40c377ce8effd0b8bdee098555d3a459b44eb1
Irgendeinwer <irgendeinwer@proton.me>
2024-08-25 11:52:35 +0200

    changed waydroid install

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -3,10 +3,10 @@
 	environment.systemPackages = with pkgs; [
 		protonup
 		mangohud
-		waydroid
 	];
 	programs = {
 		steam.enable = true;
 		gamemode.enable = true;
 	};
+	virtualisation.waydroid.enable = true;
 }
