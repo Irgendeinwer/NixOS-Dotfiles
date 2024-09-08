97fe475e21b8afe4626b4468789cb1e28eb17b0d
Irgendeinwer <irgendeinwer@proton.me>
2024-09-08 21:49:44 +0200

    fixed shutdown alias

--- hosts/junixos/home.nix
+++ hosts/junixos/home.nix
@@ -33,7 +33,7 @@
 
     			rebuild = "~/dotfiles/modules/scripts/rebuild.sh";
 
-			stfu = "git push && systemctl poweroff";
+			stfu = "git -C ~/dotfiles/ push && systemctl poweroff";
 			lock = "systemctl suspend & hyprlock";
   		};
   		history = {
