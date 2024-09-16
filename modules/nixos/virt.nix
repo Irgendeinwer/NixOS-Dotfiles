88b7f9dd7fe2f92ab9d2f03e1d68751ef8c2917f
Irgendeinwer <irgendeinwer@proton.me>
2024-09-16 22:11:51 +0200

    changed virt.nix

--- modules/nixos/virt.nix
+++ modules/nixos/virt.nix
@@ -1,6 +1,6 @@
-{ config, pkgs, ...}:
+{ config, lib, pkgs, ...}:
 {
-  boot.kernelParams = [ "intel_iommu=on" "iommu=pt" ];
+# boot.kernelParams = [ "intel_iommu=on" "iommu=pt" ];
   boot.kernelModules = [ "kvm-intel" "vfio" "vfio-pci" "vfio_iommu_type1" ];
 
   users.users.julian = {
@@ -16,6 +16,20 @@
 		runAsRoot = true;
   	};
   };
+
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  boot.kernelParams = let
+  devices = [ "1002:73ff" ]; #RX6600
+  in [
+	"intel_iommu=on" "iommu=pt" "vfio-pci.ids=${lib.concatStringsSep "," devices}"
+  ];
+
+  boot.initrd.kernelModules = [
+       	"vfio_pci"
+       	"vfio"
+       	"vfio_iommu_type1"
+  ];
 
   environment.systemPackages = with pkgs; [
 	virt-manager
