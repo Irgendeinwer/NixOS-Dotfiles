e0bf09628c816a3855cde053c84b0b582bc6822a
Irgendeinwer <irgendeinwer@proton.me>
2024-11-27 20:11:27 +0100

    nixvim: init

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@ inputs = {
 		inputs.nixpkgs.follows = "nixpkgs";
 	};
 
+	nixvim = {
+		url = "github:nix-community/nixvim";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+
 	zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
 	# hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
@@ -22,13 +27,17 @@ outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
 		specialArgs = {inherit inputs;};
 		modules = [
         		./hosts/junixos/configuration.nix
+
 			nixos-hardware.nixosModules.common-cpu-intel-cpu-only
 			nixos-hardware.nixosModules.common-pc-ssd
+
 			inputs.home-manager.nixosModules.default
 			{
             			home-manager.useGlobalPkgs = true;
             			home-manager.useUserPackages = true;
           		}
+
+			inputs.nixvim.nixosModules.nixvim
 		];
 	};
 
@@ -36,13 +45,17 @@ outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
 		specialArgs = {inherit inputs;};
 		modules = [
 			./hosts/junixbook/configuration.nix
+
 			nixos-hardware.nixosModules.common-cpu-intel
 			nixos-hardware.nixosModules.common-pc-ssd
+
 			inputs.home-manager.nixosModules.default
 			{
 				home-manager.useGlobalPkgs = true;
 				home-manager.useUserPackages = true;
 			}
+
+			inputs.nixvim.nixosModules.nixvim
 		];
 	};
 };
