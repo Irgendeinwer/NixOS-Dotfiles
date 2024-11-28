2d70f5d41a987818c0077b7508bad0cf633d7187
Irgendeinwer <irgendeinwer@proton.me>
2024-11-28 09:19:38 +0100

    nixvim: enabled nixd language server

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -1,16 +1,19 @@
 { pkgs, inputs, config, ... }:
 {
   imports = [
-	# ./plugins/default.nix
-	./options.nix
-	# ./keybinds.nix
+    ./plugins/default.nix
+    ./options.nix
+    # ./keybinds.nix
   ];
 
-
   programs.nixvim = {
-	enable = true;
-	colorschemes.gruvbox = {
-		enable = true;
-	};
+    enable = true;
+    colorschemes.gruvbox = {
+        enable = true;
+    };
   };
+
+  environment.systemPackages = with pkgs; [
+    nixd
+  ];
 }
