07230d8cd079a88f75ff8f64630d67766e40c6b5
Irgendeinwer <irgendeinwer@proton.me>
2025-01-07 16:24:57 +0100

    gaming.nix: installed itch, heroic

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,12 +1,16 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    itch
+    heroic
+    r2modman
+
     protonup
     protontricks
     mangohud
+
     ddnet
     osu-lazer-bin # "-bin" is needed to submit scores and play online multiplayer
-    r2modman
   ];
   programs = {
     steam.enable = true;
