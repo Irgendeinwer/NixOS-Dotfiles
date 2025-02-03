a122e4f7280ab53ca0f82b82add36c79175bacf6
Irgendeinwer <irgendeinwer@proton.me>
2025-02-03 15:31:52 +0100

    made wiki.js work?

--- modules/services/wiki-js.nix
+++ modules/services/wiki-js.nix
@@ -1,11 +1,19 @@
 { ... }:
 {
+  systemd.services.wiki-js = {
+    requires = [ "postgresql.service" ];
+    after = [ "postgresql.service" ];
+ };
+
   services.wiki-js = {
     enable = true;
     settings = {
-	port = 80;
+	port = 8080;
 	db = {
+	    db = "wiki-js";
 	    host = "/run/postgresql";
+	    type = "postgres";
+	    user = "wiki-js";
 	};
     };
   };
