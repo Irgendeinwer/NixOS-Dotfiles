8d6fb6a5a3daab0e44d645a5c850c28c30b68850
Irgendeinwer <irgendeinwer@proton.me>
2025-02-21 15:23:33 +0100

    zsh: modified the dl-music alias

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -20,7 +20,7 @@
 	lock = "systemctl suspend & hyprlock";
 
 	# yt-dlp aliases
-	dl-music = "yt-dlp -f bestaudio/best --extract-audio -o \"%(title)s\"";
+	dl-music = "yt-dlp -f bestaudio -x --audio-format opus --add-metadata --parse-metadata \"playlist_index:%(track_number)s\" --embed-thumbnail -o \"%(title)s.%(ext)s\"";
     };
     history = {
     	size = 10000;
