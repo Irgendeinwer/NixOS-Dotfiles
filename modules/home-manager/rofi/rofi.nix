ad104154ecb201ccd45bbdf619f435137d77d1dd
Irgendeinwer <irgendeinwer@proton.me>
2025-09-15 22:35:43 +0200

    nix flake update

--- modules/home-manager/rofi/rofi.nix
+++ modules/home-manager/rofi/rofi.nix
@@ -2,7 +2,7 @@
 {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi-wayland;
+    # package = pkgs.rofi-wayland;
     location = "center";
     theme = "~/dotfiles/modules/home-manager/rofi/gruvbox-material.rasi";
   };
