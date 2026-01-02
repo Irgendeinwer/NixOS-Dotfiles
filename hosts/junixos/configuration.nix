263b9b3d3ecf6b2a5999d9857bb74aa34bfe38bf
Irgendeinwer <irgendeinwer@proton.me>
2026-01-02 17:55:51 +0100

    junixos: added dnscrypt and mullvad-adblock DoH

--- hosts/junixos/configuration.nix
+++ hosts/junixos/configuration.nix
@@ -29,9 +29,42 @@
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "junixos";
+  # Setting up networking + dns
+  services.resolved.enable = false;
 
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "junixos";
+    networkmanager.enable = true;
+    networkmanager.dns = "none";
+    nameservers = [ "127.0.0.1" "::1" ];
+  };
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+      
+      ipv6_servers = true;
+      
+      require_dnssec = true;
+
+      # SPECIFIC MULLVAD CONFIGURATION
+      # 'mullvad-adblock-doh' is the pre-defined name for:
+      # https://adblock.dns.mullvad.net/dns-query
+      server_names = [ "mullvad-adblock-doh" ];
+      
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+    };
+  };
+
+
 
   time.timeZone = "Europe/Berlin";
 
