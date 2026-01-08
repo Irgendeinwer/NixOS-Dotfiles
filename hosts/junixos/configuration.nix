cc775c5a1ed5b7fa646bfc20cfe5ba5083752851
Irgendeinwer <irgendeinwer@proton.me>
2026-01-08 20:53:25 +0100

    nix flake update + dnscrypt rename + hyprland fix

--- hosts/junixos/configuration.nix
+++ hosts/junixos/configuration.nix
@@ -39,7 +39,7 @@
     nameservers = [ "127.0.0.1" "::1" ];
   };
 
-  services.dnscrypt-proxy2 = {
+  services.dnscrypt-proxy = {
     enable = true;
     settings = {
       listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
