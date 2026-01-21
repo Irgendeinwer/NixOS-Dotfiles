15fc8ef335a94de27c5a48111385c4aa2f61f4b8
Irgendeinwer <irgendeinwer@proton.me>
2026-01-21 13:23:58 +0100

    new zsh function: listen

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -21,9 +21,36 @@
 
       # yt-dlp aliases
       dl-music = ''yt-dlp -f bestaudio -x --audio-format opus --add-metadata --parse-metadata "playlist_index:%(track_number)s" --embed-thumbnail -o "%(title)s.%(ext)s"'';
-
-      listen = "bash -c 'nix run nixpkgs#fd -- -e opus -e flac -e mp3 -e wav -e m4a -a \"$0\" ~/music -X nix run nixpkgs#mpv -- --no-video --shuffle'";
     };
+
+    # Renamed from initExtra to initContent based on your error
+    initContent = ''
+      listen() {
+        local matches
+        matches=$(nix run nixpkgs#fd -- -e opus -e flac -e mp3 -e wav -e m4a -a . ~/music \
+                  | nix run nixpkgs#fzf -- --filter "$*")
+
+        if [ -z "$matches" ]; then
+          echo "No music found matching: $*"
+          return 1
+        fi
+
+        # 1. display-tags="": Hides the ugly monochrome "File tags" block
+        # 2. term-playing-msg: Rebuilds the stats manually with colors
+        #    - Line 1: Title (Bold Green)
+        #    - Line 2: Artist (Bold Cyan) on Album (Grey)
+        #    - Line 3: Date (Yellow) • Genre (Magenta)
+        echo "$matches" | nix run nixpkgs#mpv -- \
+            --no-video \
+            --term-osd-bar \
+            --msg-color \
+            --msg-level=playlist=error,file=error \
+            --display-tags="" \
+            --term-playing-msg=$'\n\e[1;32m''${media-title}\e[0m\n\e[1;36m''${metadata/artist} \e[0;90mon \e[0;37m''${metadata/album}\e[0m\n\e[0;33m''${metadata/date} \e[0;90m• \e[0;35m''${metadata/genre}\e[0m' \
+            --playlist=-
+      }
+    '';
+
     history = {
       size = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
