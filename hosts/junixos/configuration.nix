f3df36e0536a93565368267861cec481236b5d25
Irgendeinwer <irgendeinwer@proton.me>
2026-02-24 14:08:09 +0100

    fix(host/junixos): update dnscrypt-proxy cache file path

--- hosts/junixos/configuration.nix
+++ hosts/junixos/configuration.nix
@@ -58,7 +58,7 @@
           "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
           "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
         ];
-        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
         minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
     };
