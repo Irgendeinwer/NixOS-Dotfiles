8d91cc02ce0e312da9957693316fd01fec5891f9
Irgendeinwer <irgendeinwer@proton.me>
2026-03-23 20:07:44 +0100

    pkgs(nixos): add android-tools

--- modules/nixos/android.nix
+++ modules/nixos/android.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [ scrcpy ];
+  environment.systemPackages = with pkgs; [
+    scrcpy
+    android-tools
+  ];
+
+  users.users.julian.extraGroups = [ "adbusers" "kvm" ];
+
   virtualisation.waydroid = {
     enable = true;
     package = pkgs.waydroid-nftables;
