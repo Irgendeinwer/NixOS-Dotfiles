6658f8931106a957f2e03cb99930326e1591612b
Irgendeinwer <irgendeinwer@proton.me>
2026-03-25 13:09:58 +0100

    fix(hm/mpv): remove autoload to prevent directory leaks and enable default shuffle/loop

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -7,12 +7,15 @@
       uosc             # The best Proximity-UI (essential for minimalism)
       mpris            # Allows Hyprland/Waybar to control the player
       thumbfast        # High-speed seekbar previews (integrated with uosc)
-      autoload         # Automatically appends files in the folder to playlist
       sponsorblock     # Automatically skips sponsorships in YouTube links
       quality-menu     # uosc-style menu for selecting stream resolutions
     ];
 
     config = {
+      # --- Playback Behavior ---
+      loop-file = "inf";    	    # "inf" means infinite loop for the current file
+      shuffle = "yes";		    # Randomize the playlist on startup
+
       # --- Rendering ---
       vo = "gpu-next";              # Use the modern libplacebo-based renderer
       gpu-context = "wayland";      # Wayland-native (crucial for Hyprland)
