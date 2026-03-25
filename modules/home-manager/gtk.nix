42fa2ac140c0136a4af890d56fe0dc29bb71ba67
Irgendeinwer <irgendeinwer@proton.me>
2026-03-25 12:57:20 +0100

    fix(style/gtk): set gtk4 theme to null to fix evaluation warning

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -6,16 +6,20 @@
       package = pkgs.gruvbox-gtk-theme;
       name = "gruvbox-gtk-theme";
     };
+    
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
       gtk-button-images = true;
       gtk-menu-images = true;
     };
 
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
-      gtk-button-images = true;
-      gtk-menu-images = true;
+    gtk4 = {
+      theme = null;
+      extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+        gtk-button-images = true;
+        gtk-menu-images = true;
+      };
     };
   };
 }
