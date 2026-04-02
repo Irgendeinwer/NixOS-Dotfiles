c25acc9e24d8277a66e102e0a5dabaaa84a97914
Irgendeinwer <irgendeinwer@proton.me>
2026-04-03 01:46:57 +0200

    feat(host/junixos/networking): add 5GHz hotspot service

---
+++ modules/services/hotspot.nix
@@ -0,0 +1,60 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.networking.hotspot;
+in
+{
+  options.networking.hotspot = {
+    enable = lib.mkEnableOption "High-Speed 5GHz Hotspot";
+
+    wifiInterface = lib.mkOption {
+      type = lib.types.str;
+      description = "The name of the USB Wi-Fi interface (e.g., wlp0s20f0u7).";
+    };
+
+    ethernetInterface = lib.mkOption {
+      type = lib.types.str;
+      description = "The name of the internet source interface (e.g., enp7s0).";
+    };
+
+    ssid = lib.mkOption {
+      type = lib.types.str;
+      default = "6-7";
+    };
+
+    password = lib.mkOption {
+      type = lib.types.str;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    boot.kernelParams = [ "usbcore.autosuspend=-1" ];
+    boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+    hardware.wirelessRegulatoryDatabase = true;
+
+    systemd.services.hotspot = {
+      description = "High-Speed 5GHz Hotspot";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" "NetworkManager.service" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStartPre = "${pkgs.iw}/bin/iw reg set DE";
+        ExecStart = ''
+          ${pkgs.linux-wifi-hotspot}/bin/create_ap \
+            --ieee80211n --ieee80211ac \
+            --ht_capab '[HT40+][SHORT-GI-20][SHORT-GI-40]' \
+            --vht_capab '[MAX-A-MPDU-LEN-EXP-7][VHT80][SHORT-GI-80][RX-STBC1]' \
+            --freq-band 5 -c 36 \
+            ${cfg.wifiInterface} ${cfg.ethernetInterface} "${cfg.ssid}" "${cfg.password}"
+        '';
+        ExecStop = "${pkgs.linux-wifi-hotspot}/bin/create_ap --stop ${cfg.wifiInterface}";
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+    };
+
+    networking.networkmanager.unmanaged = [ "interface-name:${cfg.wifiInterface}" ];
+
+    environment.systemPackages = [ pkgs.linux-wifi-hotspot pkgs.iw ];
+  };
+}
