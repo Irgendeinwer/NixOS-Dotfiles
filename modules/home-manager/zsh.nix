a44cc68e447472d4db4b4914ed9b8f32419e451f
Irgendeinwer <irgendeinwer@proton.me>
2026-05-27 10:27:46 +0200

    refactor(hm/zsh): remove unused script and keybinds

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -13,12 +13,6 @@
       home = "cd ~/dotfiles/ && nvim hosts/$HOST/home.nix";
       hypr = "cd ~/dotfiles/ && nvim modules/home-manager/hypr/";
 
-      rb = "~/dotfiles/modules/scripts/rebuild.sh";
-      rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles/#$HOST";
-
-      stfu = "git -C ~/dotfiles/ push && systemctl poweroff";
-      lock = "systemctl suspend & hyprlock";
-
       # yt-dlp aliases
       dl-music = ''yt-dlp -f bestaudio -x --audio-format opus --add-metadata --parse-metadata "playlist_index:%(track_number)s" --embed-thumbnail -o "%(title)s.%(ext)s"'';
     };
