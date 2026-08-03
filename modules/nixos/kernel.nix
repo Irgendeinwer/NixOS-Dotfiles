52d4cdee0bd8b230e8c16d7de0fc3f4e290a6365
Irgendeinwer <irgendeinwer@proton.me>
2026-08-03 12:14:06 +0200

    chore(nixos/kernel): remove garnix substituter as service shut down

--- modules/nixos/kernel.nix
+++ modules/nixos/kernel.nix
@@ -34,14 +34,8 @@ in
         inputs.nix-cachyos-kernel.legacyPackages.${pkgs.stdenv.hostPlatform.system}.linuxPackages-cachyos-latest;
 
       nix.settings = {
-        substituters = [
-          "https://attic.xuyh0120.win/lantian"
-          "https://cache.garnix.io"
-        ];
-        trusted-public-keys = [
-          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        ];
+        substituters = [ "https://attic.xuyh0120.win/lantian" ];
+        trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
       };
     })
   ];
