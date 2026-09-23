2b5c7c5e407ac6463649bd09e3bb7e1c6b99a2f0
Irgendeinwer <irgendeinwer@proton.me>
2026-09-23 17:19:43 +0200

    refactor(hm/rofi): use relative path for theme

--- modules/home-manager/rofi/rofi.nix
+++ modules/home-manager/rofi/rofi.nix
@@ -1,9 +1,6 @@
-{ pkgs, ... }:
 {
   programs.rofi = {
     enable = true;
-    # package = pkgs.rofi-wayland;
-    location = "center";
-    theme = "~/dotfiles/modules/home-manager/rofi/gruvbox-material.rasi";
+    theme = ./gruvbox-material.rasi;
   };
 }
